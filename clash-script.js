1c21573712129eb4e2437d41c6d14867989b9ca4
Niklaus88 <102349810+Niklaus88@users.noreply.github.com>
2026-08-20 15:59:22 +0800

    style: reorder proxy groups (Netflix below TikTok, Spotify below Telegram)

--- clash-script.js
+++ clash-script.js
@@ -290,19 +290,19 @@ function main(config) {
     },
     {
       ...groupBaseOption,
-      "name": "Netflix",
-      "type": "select",
-      "proxies": ["节点选择","全局直连"],
-      "include-all": true,
-      "icon": "https://raw.githubusercontent.com/Koolson/Qure/refs/heads/master/IconSet/Color/Netflix.png"
-    },
-    {
-      ...groupBaseOption,
       "name": "Telegram",
       "type": "select",
       "proxies": ["节点选择","全局直连"],
       "include-all": true,
       "icon": "https://raw.githubusercontent.com/Koolson/Qure/refs/heads/master/IconSet/Color/Telegram.png"
+    },
+    {
+      ...groupBaseOption,
+      "name": "Spotify",
+      "type": "select",
+      "proxies": ["节点选择","全局直连"],
+      "include-all": true,
+      "icon": "https://raw.githubusercontent.com/Koolson/Qure/refs/heads/master/IconSet/Color/Spotify.png"
     },
     {
       ...groupBaseOption,
@@ -319,6 +319,14 @@ function main(config) {
       "include-all": true,
       "proxies": ["节点选择"],
       "icon": "https://raw.githubusercontent.com/Koolson/Qure/refs/heads/master/IconSet/Color/TikTok.png"
+    },
+    {
+      ...groupBaseOption,
+      "name": "Netflix",
+      "type": "select",
+      "proxies": ["节点选择","全局直连"],
+      "include-all": true,
+      "icon": "https://raw.githubusercontent.com/Koolson/Qure/refs/heads/master/IconSet/Color/Netflix.png"
     },
     {
       ...groupBaseOption,
@@ -356,14 +364,6 @@ function main(config) {
     },
     {
       ...groupBaseOption,
-      "name": "Spotify",
-      "type": "select",
-      "proxies": ["节点选择","全局直连"],
-      "include-all": true,
-      "icon": "https://raw.githubusercontent.com/Koolson/Qure/refs/heads/master/IconSet/Color/Spotify.png"
-    },
-    {
-      ...groupBaseOption,
       "name": "广告过滤",
       "type": "select",
       "proxies": ["REJECT", "DIRECT"],
@@ -395,7 +395,7 @@ function main(config) {
     }
   ];
 
-  // 覆盖原配置中的规则
+// 覆盖原配置中的规则
   config["rule-providers"] = ruleProviders;
   config["rules"] = rules;
 // 添加判断
