e511b9d3dd3a07992ded424494bc5adf101a2104
Niklaus88 <102349810+Niklaus88@users.noreply.github.com>
2026-09-16 00:44:31 +0800

    fix(rules): route Google domain suffixes to Google proxy group in clash-script.js

--- clash-script.js
+++ clash-script.js
@@ -200,9 +200,11 @@ const rules = [
   "DOMAIN-SUFFIX,browserleaks.com,节点选择",
   "DOMAIN-SUFFIX,browserleaks.org,节点选择",
   "DOMAIN-SUFFIX,ipleak.net,节点选择",
-  "DOMAIN-SUFFIX,googleapis.cn,节点选择", // Google服务
-  "DOMAIN-SUFFIX,gstatic.com,节点选择", // Google静态资源
-  "DOMAIN-SUFFIX,xn--ngstr-lra8j.com,节点选择", // Google Play下载服务
+  "PROCESS-NAME,tune_free_next.exe,DIRECT", // TuneFreeNext 服务
+  "DOMAIN-SUFFIX,sayqz.com,DIRECT", // TuneFreeNext 服务
+  "DOMAIN-SUFFIX,googleapis.cn,谷歌服务", // Google服务
+  "DOMAIN-SUFFIX,gstatic.com,谷歌服务", // Google静态资源
+  "DOMAIN-SUFFIX,xn--ngstr-lra8j.com,谷歌服务", // Google Play下载服务
   "DOMAIN-SUFFIX,github.io,节点选择", // Github Pages
   "DOMAIN,v2rayse.com,节点选择", // V2rayse节点工具
   // Loyalsoldier 规则集
