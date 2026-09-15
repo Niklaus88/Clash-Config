bbaf5579b40fd2602702b473615862b79f0ec6b0
Niklaus88 <102349810+Niklaus88@users.noreply.github.com>
2026-09-16 00:44:39 +0800

    fix(rules): route Google domain suffixes to Google proxy group in clash-ios-script.js

--- clash-ios-script.js
+++ clash-ios-script.js
@@ -92,8 +92,9 @@ const rules = [
   "DOMAIN-SUFFIX,browserleaks.com,节点选择",
   "DOMAIN-SUFFIX,browserleaks.org,节点选择",
   "DOMAIN-SUFFIX,ipleak.net,节点选择",
-  "DOMAIN-SUFFIX,googleapis.cn,节点选择",
-  "DOMAIN-SUFFIX,gstatic.com,节点选择",
+  "DOMAIN-SUFFIX,sayqz.com,DIRECT",
+  "DOMAIN-SUFFIX,googleapis.cn,谷歌服务",
+  "DOMAIN-SUFFIX,gstatic.com,谷歌服务",
   "DOMAIN-SUFFIX,github.io,节点选择",
 
   // 4. 内置轻量 GEOSITE 专业分流
