47fbc7861a07cd5aeaaa61ee789539d896db023b
Niklaus88 <102349810+Niklaus88@users.noreply.github.com>
2026-09-18 14:08:33 +0800

    perf(apple): set DIRECT as default proxy for Apple services in clash-script.js

--- clash-script.js
+++ clash-script.js
@@ -352,7 +352,7 @@ function main(config) {
       ...groupBaseOption,
       "name": "苹果服务",
       "type": "select",
-      "proxies": ["节点选择", "全局直连"],
+      "proxies": ["全局直连", "节点选择"],
       "include-all": true,
       "icon": "https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Apple.png"
     },
